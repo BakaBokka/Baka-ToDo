8b50273576e79bbd84c837166624ea093e83c289
Damir Vakhitov <damirvakhitov@gmail.com>
2020-10-26 23:42:52 +0300

    fix: fix empty localstorage mapping

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 import Task from "./components/Task";
 import "./ToDoList.scss";
 
-
-
 const ToDoList = () => {
-  const todosData = JSON.parse(localStorage.getItem('todos'))
+  const todosData = JSON.parse(localStorage.getItem("todos"));
   const [value, setValue] = useState("");
   const [todos, setTodos] = useState(todosData);
-
-
+console.log(localStorage)
   const handleChange = ({ target }) => {
     setValue(target.value);
   };
@@ -21,39 +18,46 @@ const ToDoList = () => {
   };
 
   const addTodo = (text) => {
-   localStorage.setItem('todos', JSON.stringify([...todos, text]));
-   setTodos(JSON.parse(localStorage.getItem('todos')));
+    localStorage.setItem("todos", JSON.stringify([...todos, text]));
+
+    todos !== null ?  setTodos(JSON.parse(localStorage.getItem("todos"))) :  setTodos([]);
   };
 
   const deleteTodo = (index) => {
     const newTodos = todos.filter((task, i) => i !== index);
-    localStorage.setItem('todos', JSON.stringify(newTodos));
-    setTodos(JSON.parse(localStorage.getItem('todos')));
-  }
+    localStorage.setItem("todos", JSON.stringify(newTodos));
 
-  let taskElement = todos.map((task, i) => <Task task={task} key={i} delete={deleteTodo} index={i}/>
-  )
+    setTodos(JSON.parse(localStorage.getItem("todos")));
+  };
+
+
+  let taskElement;
+
+  if( todos !== null) {
+    taskElement = todos.map((task, i) => (
+    <Task task={task} key={i} delete={deleteTodo} index={i} />
+  ));
+  } else taskElement = [];
 
   return (
     <section className="todo">
       <div className="todo__content">
-      <h1 className="todo__title">Baka ToDos</h1>
-      <form className="todo__form" onSubmit={handleSubmit}>
-        <input
-          className="todo__input"
-          type="text"
-          autoFocus
-          value={value}
-          placeholder="What's my task?"
-          onChange={handleChange}
+        <h1 className="todo__title">Baka ToDos</h1>
+        <form className="todo__form" onSubmit={handleSubmit}>
+          <input
+            className="todo__input"
+            type="text"
+            autoFocus
+            value={value}
+            placeholder="What's my task?"
+            onChange={handleChange}
+            required
+            // minLength="1"
+          />
+        </form>
 
-        />
-      </form>
-
-      <ul className="todo__tasks">
-        {taskElement}
-        </ul>
-        </div>
+        <ul className="todo__tasks">{taskElement}</ul>
+      </div>
     </section>
   );
 };
